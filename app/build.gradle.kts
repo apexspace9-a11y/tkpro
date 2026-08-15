3e201b972bbf462b10de24a0165ab95e58e8958b
apexspace9-a11y <apexspace9@gmail.com>
2026-08-15 23:08:23 +0700

    V4 nâng version Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "vn.tietkiem.pro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 4
+        versionName = "4.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
